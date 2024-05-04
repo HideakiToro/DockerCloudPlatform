c2a6020bdf92b990369608132aae975b684dc128
HideakiToro <hideaki.toro@gmail.com>
2024-05-05 01:45:21 +0200

    Overview and Details

---
+++ Frontend/pages/Status.vue
@@ -0,0 +1,113 @@
+<template>
+    <div class="NavBar">
+        Container Info
+        <div class="Profil"></div>
+    </div>
+    <div class="StatusContainer">
+        <div class="StatusName">The Container Name</div>
+        <div class="StatusType">Intern</div>
+        <div class="StatusStatus"></div>
+        <div class="StatusLogs">
+            <div v-for="(item, index) in 59">
+                <div v-if="Math.random() > 0.2">[05/05/2024 01:25:{{ item }}] Server 1: No Issues detected.</div>
+                <div v-else>[05/05/2024 01:25:{{ item }}] Server 1: Some Issues detected that might require human intervention.</div>
+            </div>
+        </div>
+        <div class="StatusInfo">
+            <div class="StatusInfoTitle">Connection</div>
+            IP: 127.0.0.1:4000<br>
+            URL: 127.0.0.1:3000/pods?id=15
+        </div>
+    </div>
+</template>
+
+<style>
+.StatusContainer {
+    position: relative;
+    margin: auto;
+    background-color: rgb(35, 35, 45);
+    min-width: 200pt;
+    width: calc(50vw - 50pt);
+    height: calc(100vh - 130pt);
+    margin-top: 15pt;
+    border-radius: 15pt;
+    padding: 25pt;
+    justify-content: center;
+}
+
+.StatusName {
+    text-align: center;
+    font-size: 25pt;
+    font-weight: 600;
+    overflow: auto;
+    white-space: nowrap;
+}
+
+.StatusType {
+    text-align: center;
+    margin-top: 10pt;
+}
+
+.StatusStatus {
+    position: absolute;
+    top: 15pt;
+    right: 15pt;
+    width: 50pt;
+    height: 50pt;
+    background-color: rgb(70, 255, 110);
+    border-radius: 25pt;
+}
+
+.StatusLogs {
+    line-height: 25pt;
+    position: absolute;
+    margin: auto;
+    border: solid 2pt rgb(155, 255, 170);
+    border-radius: 15pt;
+    padding: 15pt;
+    top: 100pt;
+    bottom: 25%;
+    left: 12.5%;
+    right: 12.5%;
+    background-color: rgb(35, 35, 35);
+    color: white;
+    overflow: auto;
+}
+
+.StatusLogs::-webkit-scrollbar {
+    background: transparent;
+}
+
+.StatusLogs::-webkit-scrollbar-thumb {
+    background: rgb(155, 255, 170);
+    border-radius: 5pt;
+}
+
+.StatusInfo {
+    position: absolute;
+    text-align: center;
+    border: solid 2pt rgb(155, 255, 170);
+    border-radius: 15pt;
+    padding: 15pt;
+    margin: auto;
+    bottom: 25pt;
+    left: 12.5%;
+    right: 12.5%;
+    height: 12.5%;
+    overflow: auto;
+}
+
+.StatusInfo::-webkit-scrollbar {
+    background: transparent;
+}
+
+.StatusInfo::-webkit-scrollbar-thumb {
+    background: rgb(155, 255, 170);
+    border-radius: 5pt;
+}
+
+.StatusInfoTitle {
+    margin-bottom: 15pt;
+    text-decoration: underline;
+}
+</style>
--- Frontend/pages/index.vue
+++ Frontend/pages/index.vue
@@ -1,3 +1,132 @@
 <template>
-  Hello World
+  <div class="NavBar">
+    Your Containers
+    <div class="Profil"></div>
+  </div>
+  <div class="CardGrid">
+    <div class="Card" v-for="(item, index) in 50" @click="navigateDetails()">
+      <div class="CardTitle">This is Container #{{ index }}</div>
+      <div v-if="Math.random() > 0.8" class="CardStatus"></div>
+      <div v-else class="CardStatus OkStatus"></div>
+    </div>
+  </div>
 </template>
+
+<style>
+body {
+  background-color: rgb(55, 55, 70);
+  color: rgb(155, 255, 170);
+  font-family: Roboto;
+  font-size: 16pt;
+  font-weight: 500;
+  z-index: 1;
+  padding: 0;
+  margin: 0;
+  height: 100%;
+}
+
+.NavBar {
+  width: calc(100%-30pt);
+  height: 50pt;
+  align-content: center;
+  padding-left: 15pt;
+  padding-right: 15pt;
+  background-color: rgb(35, 35, 45);
+  z-index: 2;
+  border-bottom: solid 3pt rgb(155, 255, 170);
+  position: sticky;
+  top: 0;
+}
+
+.Profil {
+  position: absolute;
+  top:5pt;
+  right: 25pt;
+  width: 32pt;
+  height: 32pt;
+  border-radius: 20pt;
+  border: solid 3pt rgb(155, 255, 170);
+}
+
+.Profil:hover {
+  animation: hoverProfil 0.05s linear forwards;
+}
+
+@keyframes hoverProfil {
+  from {
+    background-color: transparent;
+  }
+  to {
+    background-color: rgb(155, 255, 170);
+  }
+}
+
+.CardGrid {
+  width: 100%;
+  height: 100%;
+  margin-top: 12.5pt;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, 250pt);
+  place-content: center;
+}
+
+.Card {
+  width: 150pt;
+  height: 100pt;
+  padding: 25pt;
+  padding-bottom: 22pt;
+  margin-top: 12.5pt;
+  margin-bottom: 12.5pt;
+  background-color: rgb(35, 35, 45);
+  border-radius: 10pt;
+  text-align: center;
+  border-bottom: solid 3pt rgb(155, 255, 170);
+  overflow: hidden;
+}
+
+.Card:hover {
+  animation: hoverCard 0.05s ease-in forwards;
+}
+
+@keyframes hoverCard {
+  from {
+    border-bottom: solid 3pt rgb(155, 255, 170);
+    padding-bottom: 22pt;
+  }
+  to {
+    border-bottom: solid 15pt rgb(155, 255, 170);
+    padding-bottom: 10pt;
+  }
+}
+
+.CardTitle {
+  height: 40pt;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.CardStatus {
+  color: white;
+  margin-top: 20pt;
+  padding-top: 10pt;
+  background-color: rgb(255, 70, 70);
+  height: 30pt;
+  border-radius: 15pt;
+  position: inherit;
+  bottom: 0;
+}
+
+.OkStatus {
+  background-color: rgb(70, 255, 110);
+}
+</style>
+
+<script>
+export default {
+  methods: {
+    navigateDetails() {
+      window.open("/Status", "_self")
+    }
+  }
+}
+</script>
