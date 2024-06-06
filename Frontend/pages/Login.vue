2f2939f33f9d9fd76e2c7438d6fe27913840e314
Okan Adiguezel <okan.adiguezel@icloud.com>
2024-06-06 20:28:38 +0200

    added Register.vue and login.vue with loginWindow

---
+++ Frontend/pages/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="LoginPage">
+        Welcome
+    </div>
+</template>
+
+<style scoped>
+.LoginPage {
+    position: fixed;
+    width: 400pt;
+    height: 300pt;
+    top: calc(50vh - 150pt);
+    left: calc(50vw - 200pt);
+    background-color:rgb(35, 35, 45) ;
+    border-radius: 25pt;
+    text-align: center;
+    align-content: center;
+}
+</style>
